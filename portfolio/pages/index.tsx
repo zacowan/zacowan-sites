fa14400854d42cf815c5f1ae16a5eaf9a6d3a174
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-01-21 14:23:06 -0500

    feat(init): initialized Next.js and tailwindcss

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="m-2 top-4 left-4">
+      <h1 className="text-blue-500 text-4xl">Portfolio.</h1>
+    </div>
+  );
+};
+
+export default Home;
